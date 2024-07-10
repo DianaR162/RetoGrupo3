e8347b9d958a415e05c0dd21e1b3aef8dd5b1e03
Diana Roldan <dianaroldanr162@gmail.com>
2024-07-10 14:51:40 -0500

    Se crean componentes paginas vacios y se migran los recursos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingPageComponent },
+    { path: 'log-in', component: LogInComponent },
+    { path: 'register', component: RegisterComponent }
+];
---
+++ src/app/dashboard/diagnosis/diagnosis.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diagnosis',
+  standalone: true,
+  imports: [],
+  templateUrl: './diagnosis.component.html',
+  styleUrl: './diagnosis.component.css'
+})
+export class DiagnosisComponent {
+
+}

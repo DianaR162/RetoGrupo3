bec2cdc10a3eefd3161335ab6dd5dc14fb5cf4f9
dxmalagon <dxmalagon@gmail.com>
2024-07-11 21:55:06 -0500

    Migracion form y welcome

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { RegisterComponent } from './register/register.component';
+import { FormComponent } from './form/form.component';
+import { WelcomeComponent } from './form/welcome/welcome.component';
+
+
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
     { path: 'log-in', component: LogInComponent },
-    { path: 'register', component: RegisterComponent }
+    { path: 'register', component: RegisterComponent },
+    { path: 'form', component: FormComponent },
+    { path: 'welcome', component: WelcomeComponent }
 ];

597c5234cb51f4fa1446ee2176e462d59d8e3592
Diana Roldan <dianaroldanr162@gmail.com>
2024-07-17 00:25:38 -0500

    se realiza logica js formulario

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,27 +1,56 @@
 import { Component } from '@angular/core';
 import { IQuestion, QuestionsService } from '../services/nodo-reto1-api/questions.service';
-import { Observable } from 'rxjs';
+import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [NgFor, RouterLink],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  currentQuestion: number = 1;
+  nextQuestion: number = 2;
+  prevQuestion: number = 0;
+  totalQuestions: number = 0;
   questions!: IQuestion[];
 
   constructor(private questionsService: QuestionsService) {}
 
-  ngAfterViewInit(): void {
+  private getQuestions(): void {
     this.questionsService.getQuestions().subscribe({
       next: (response) => {
         this.questions = response
+        this.totalQuestions = response.length
       },
       error: (error) => {
         console.error(error);
       }
-    })
+    });
+  }
+
+  updateSliderLabel(id: number): void {
+    const sliderLabel = document.getElementById('sliderLabel-' + id) as HTMLElement;
+    const slider = document.getElementById('slider-' + id) as HTMLInputElement;
+
+    sliderLabel.textContent = slider.value;
+  }
+
+  showNextQuestion(): void {
+    this.currentQuestion++;
+    this.nextQuestion++;
+    this.prevQuestion++;
+  }
+
+  showPrevQuestion(): void {
+    this.currentQuestion--;
+    this.nextQuestion--;
+    this.prevQuestion--;
+  }
+
+  ngOnInit(): void {
+    this.getQuestions();
   }
 }

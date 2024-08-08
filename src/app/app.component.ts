f3d5395b3814cf7f8b5630462d4c4a6a134b39d2
Maria9811 <julianamarinp64@gmail.com>
2024-08-08 15:05:15 -0500

    Resolución de conflictos y actualización de animaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,7 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit() {
+  }
+}
